3e502df44ef9530efc811dd93de3f88ce0d6a7fe
Matej Ruzic da Silva Pereira <matej.gamer01@gmail.com>
2026-02-28 16:13:50 +0100

    fix broken config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://javiluma.github.io",
+  url: "https://github.com/JaViLuMa/isometric-react",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/isometric-react/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
